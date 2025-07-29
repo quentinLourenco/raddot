c4c22540cd55379919cdd8699057e5091635bc82
Corentin TURGIS <corentin.turgis@outlook.fr>
2025-07-29 13:29:56 +0200

    Add profile page

--- raddot/urls.py
+++ raddot/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
+from user_manager.views.profile.profile import profile
 
 def root_redirect(request):
     if request.user.is_authenticated:
@@ -12,5 +13,6 @@ urlpatterns = [
     path('', root_redirect, name='root_redirect'),
     path('home/', include(('social_app.urls', 'social_app'), namespace='social_app')),
     path('auth/', include(('user_manager.urls', 'user_manager'), namespace='user_manager')),
+    path('profile/', profile, name='profile'),
     path('admin/', admin.site.urls),
 ]
