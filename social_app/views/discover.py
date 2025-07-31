5af15fda56825c2bdd14330cc16741490abb1e9c
quentin lourenco <quentin.lourenco@livecampus.tech>
2025-07-31 10:32:31 +0200

    bug fixe home page + css + barre de recherche dans discorver

--- social_app/views/discover.py
+++ social_app/views/discover.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
 from social_app.models.Subraddot import Subraddot
+from django.db.models import Q
 
 def discover_subraddots(request):
     filter_type = request.GET.get('filter', 'all')
+    search_query = request.GET.get('q', '').strip()
 
     if filter_type == 'created' and request.user.is_authenticated:
-        subraddots = Subraddot.objects.filter(creator=request.user).order_by('-created_at')
+        subraddots = Subraddot.objects.filter(creator=request.user)
     elif filter_type == 'joined' and request.user.is_authenticated:
-        subraddots = request.user.joined_subraddots.all().order_by('-created_at')
+        subraddots = request.user.joined_subraddots.all()
     else:
-        subraddots = Subraddot.objects.all().order_by('-created_at')
+        subraddots = Subraddot.objects.all()
+
+    if search_query:
+        subraddots = subraddots.filter(name__icontains=search_query)
+
+    subraddots = subraddots.order_by('-created_at')
 
     context = {
         'subraddots': subraddots,
