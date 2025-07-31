f1e4c66557f88370820ae607fe28e04d81063384
quentin lourenco <quentin.lourenco@livecampus.tech>
2025-07-31 13:01:47 +0200

    model trophé

---
+++ social_app/models/Trophy.py
@@ -0,0 +1,11 @@
+from django.db import models
+from user_manager.models import User
+from social_app.models.Subraddot import Subraddot
+
+class Trophy(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='trophies')
+    subraddot = models.ForeignKey(Subraddot, on_delete=models.CASCADE, related_name='trophies')
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name} - {self.user.username} ({self.subraddot.name})"
